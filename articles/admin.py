6079139ffcab60134238e4c26fee1bf483aa3831
SuyogSky <shakyasuyog214@gmail.com>
2023-07-25 13:26:49 +0545

    First Commit

---
+++ articles/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from .models import Article, Comment
+
+
+class CommentInLine(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInLine,
+    ]
+
+# Register your models here.
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
